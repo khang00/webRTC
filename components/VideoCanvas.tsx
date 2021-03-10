d9a102387e5c55e6af0a75e835708446357894b7
khang00 <dlqkhang8@gmail.com>
2021-03-10 20:49:53 +0700

    add prettier

---
+++ components/VideoCanvas.tsx
@@ -0,0 +1,10 @@
+const VideoCanvas = props => {
+  return (<div className="card" style={{textAlign: "center"}}>
+    <div className="card-body">
+      <p className="card-text">{props.title}</p>
+      <h3 className="card-title">{props.quantity}</h3>
+    </div>
+  </div>)
+}
+
+export default VideoCanvas

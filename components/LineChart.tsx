eeb51b606ffa01c2f6d1232c85b1f9fae065865e
khang00 <dlqkhang8@gmail.com>
2021-03-11 21:43:40 +0700

    fix line charts use window which is not compatible with Static Site generation and SSR of nextjs. Moving to using nivo which does not use window fix the problem

--- components/LineChart.tsx
+++ components/LineChart.tsx
@@ -1,109 +1,99 @@
 import React from "react";
-import dynamic from "next/dynamic";
+import styles from "../styles/LineChart.module.css"
 
-const ReactApexChart = dynamic(() => import("react-apexcharts"));
+// @ts-ignore
+import { ResponsiveLine } from "@nivo/line";
+
+interface Point {
+  x: number | string | Date;
+  y: number | string | Date;
+}
+
+interface Dataset {
+  id: String | number;
+  data: Array<Point>;
+}
+
+type UnixSecond = number;
+
+const random = () => Math.floor(Math.random() * 100);
+
+const fetchData = (start: UnixSecond, duration: UnixSecond): Dataset => {
+  const interval = 3600;
+  const size = Math.floor(duration / interval);
+  const data = Array.from(Array(size), (item, index) => {
+    return { x: new Date(start + index * interval * 1000).getHours(), y: random() };
+  });
+
+  return {
+    id: start,
+    data: data,
+  };
+};
 
 export default class LineChart extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-
-    let hoursArray: any = [];
-    for (let index = 0; index < 23; index++) {
-      hoursArray.push(index);
-    }
-
     this.state = {
-      series: [],
-      options: {
-        chart: {
-          height: 350,
-          type: "line",
-          dropShadow: {
-            enabled: true,
-            color: "#000",
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 0.2,
-          },
-        },
-        colors: ["#77B6EA", "#545454"],
-        dataLabels: {
-          enabled: false,
-        },
-        stroke: {
-          curve: "smooth",
-        },
-        title: {
-          text: "Comparision between today and yesterday",
-          align: "center",
-        },
-        grid: {
-          borderColor: "#e7e7e7",
-          row: {
-            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-            opacity: 0.5,
-          },
-        },
-        markers: {
-          size: 1,
-        },
-        xaxis: {
-          categories: hoursArray,
-          title: {
-            text: "Hours",
-          },
-        },
-        yaxis: {
-          title: {
-            text: "Number of users",
-          },
-          min: 0,
-          max: 150,
-        },
-        legend: {
-          position: "top",
-          horizontalAlign: "right",
-          floating: true,
-          offsetY: -25,
-          offsetX: -5,
-        },
-      },
+      dataset: [],
     };
   }
 
   componentDidMount() {
-    let todayArray = [];
-    let yesterdayArray = [];
-    for (let index = 0; index < 23; index++) {
-      todayArray.push(Math.floor(Math.random() * Math.floor(100)));
-      yesterdayArray.push(Math.floor(Math.random() * Math.floor(100)));
-    }
-
-    //get room data from Backend
-    let dataFromBackend = [
-      {
-        name: "Today",
-        data: todayArray,
-      },
-      {
-        name: "Yesterday",
-        data: yesterdayArray,
-      },
-    ];
-    this.setState({ series: dataFromBackend });
+    const now = Math.floor(Date.now() / 3600);
+    const aDay = 3600 * 24;
+    const today = fetchData(now, aDay);
+    const yesterday = fetchData(now - aDay, aDay);
+    today.id = "today"
+    yesterday.id = "yesterday"
+    this.setState({ dataset: [yesterday, today] });
   }
 
   render() {
-    return (
-      <div className="line-chart">
-        <ReactApexChart
-          options={this.state.options}
-          series={this.state.series}
-          type="line"
-          height={500}
-        />
-      </div>
-    );
+    return <div className={styles.chart}>
+      <ResponsiveLine
+        data={this.state.dataset}
+        margin={{ top: 50, right: 100, bottom: 50, left: 50 }}
+        xScale={{ type: 'point' }}
+        yScale={{ type: 'linear' }}
+        colors={{ scheme: 'nivo' }}
+        useMesh={true}
+        axisBottom={{
+          orient: 'bottom',
+          legend: 'Hours',
+          legendOffset: 36,
+          legendPosition: 'middle'
+        }}
+        axisLeft={{
+          orient: 'left',
+          legend: 'Counts',
+          legendOffset: -40,
+          legendPosition: 'middle'
+        }}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]}
+      />
+    </div>;
   }
 }
+

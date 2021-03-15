756f3665435caad75479a79ce69caa863f0a6550
khang00 <dlqkhang8@gmail.com>
2021-03-15 16:54:50 +0700

    lint

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import { Point } from "../utils/data";
 
 const Summary = (props) => {
-  const {dataset, bandWidth, device} = props
+  const { dataset, bandWidth, device } = props;
 
-  const sortedData: Array<Point> = [...(dataset.data)].sort((a, b) => {
+  const sortedData: Array<Point> = [...dataset.data].sort((a, b) => {
     return a.y - b.y;
   });
 
-  const maxTime :string = sortedData[sortedData.length - 1].x.toString();
-  const meanTime :string = sortedData[Math.floor(sortedData.length / 2)].x.toString();
+  const maxTime: string = sortedData[sortedData.length - 1].x.toString();
+  const meanTime: string = sortedData[
+    Math.floor(sortedData.length / 2)
+  ].x.toString();
 
   return (
     <div className="container-fluid">

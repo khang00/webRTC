4a70f745204b9176670381bfe651d9ef56614012
khang00 <dlqkhang8@gmail.com>
2021-03-26 14:54:37 +0700

    lint

--- server.ts
+++ server.ts
@@ -6,8 +6,8 @@ const io = require("socket.io")(server, {
   pingTimeout: "3000",
   cookie: false,
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 const next = require("next");
 
@@ -19,52 +19,56 @@ const nextHandler = nextApp.getRequestHandler();
 const LOGIN_USER = new Map();
 const ROOMS = new Map();
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   socket.emit("connection", "");
-  socket.on("login", username => {
+  socket.on("login", (username) => {
     LOGIN_USER.set(username, socket.id);
     console.log(`${username} login`);
   });
 
-  socket.on("getRoomDetails", data => {
+  socket.on("getRoomDetails", (data) => {
     const room = ROOMS.get(data.to);
     socket.emit("roomDetails", room);
-    console.log("get room details: ", JSON.stringify(room), JSON.stringify(ROOMS));
+    console.log(
+      "get room details: ",
+      JSON.stringify(room),
+      JSON.stringify(ROOMS)
+    );
   });
 
-  socket.on("joinRoom", data => {
+  socket.on("joinRoom", (data) => {
     socket.join(data.to);
     ROOMS.get(data.to).push(data.from);
     console.log(`join room: ${ROOMS.toString()}`);
   });
 
-  socket.on("createRoom", data => {
+  socket.on("createRoom", (data) => {
     socket.join(data.to);
     ROOMS.get(data.to).push(data.from);
     console.log(`join room: ${ROOMS.toString()}`);
   });
 
-  socket.on("createRoom", data => {
+  socket.on("createRoom", (data) => {
     socket.join(data.from);
     ROOMS.set(data.from, [data.from]);
     console.log(`create room: ${ROOMS.toString()}`);
   });
 
-  socket.on("signal", data => {
+  socket.on("signal", (data) => {
     io.to(LOGIN_USER.get(data.to)).emit("signal", data);
   });
 
-  socket.on("initiate", data => {
+  socket.on("initiate", (data) => {
     io.to(LOGIN_USER.get(data.to)).emit("initiate", data);
   });
 
-  socket.on("disconnecting", reason => {
+  socket.on("disconnecting", (reason) => {
     console.log(reason);
     LOGIN_USER.forEach((username, socketId) => {
       if (socketId === socket.id) {
         LOGIN_USER.delete(username);
       }
-    })
+    });
   });
 });
 
